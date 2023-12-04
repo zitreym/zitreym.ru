b5cd34838bbce6b1dfa3dd9d070549723d8bc7e8
BaranovD <baranovd@ro.ru>
2023-12-04 23:53:38 +0300

    123

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if (!empty($data['message']['text'])) {
 			'sendMessage', 
 			array(
 				'chat_id' => $data['message']['chat']['id'],
-				'text' => ''. $otvet .''
+				'text' => ''. var_dump($data) .''
 			)
 		);
  
